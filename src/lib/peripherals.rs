891681dcaae9bce2ffd2e0fc671e0c3fec8ce6f4
Mike Salmela <salmela.mike@gmail.com>
2023-02-01 23:41:48 +0200

    Continue graphics

--- src/lib/peripherals.rs
+++ src/lib/peripherals.rs
@@ -5,26 +5,51 @@ extern crate piston;
 
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventSettings, Events};
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 
 pub struct Peripherals {
     pub gl: GlGraphics,
     pub window: Window,
+    pub grid: Vec<Vec<bool>>,
+    pub width: usize,
+    pub height: usize,
+    pub pixel_size: usize,
 }
 
 impl Peripherals {
 
     pub fn new() -> Peripherals {
         let opengl = OpenGL::V3_1;
-        let window: Window = WindowSettings::new("spinning-square", [200, 200])
-            .graphics_api(opengl)
-            .exit_on_esc(true)
-            .build()
-            .unwrap();
+        let width = 64 as usize;
+        let height = 32 as usize;
+        let pixel_size = 10;
 
-        let peripherals = Peripherals{gl: GlGraphics::new(opengl), window: window};
+        let window: Window = WindowSettings::new(
+            "rust-chip8",
+            [(width * pixel_size) as f64, (height * pixel_size) as f64]
+        )
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+        let grid = vec![vec![false; height]; width];
+
+        let peripherals = Peripherals{gl: GlGraphics::new(opengl), window: window, grid: grid,
+                                                   width: width, height: height, pixel_size: pixel_size};
         peripherals
+    }
+
+    pub fn draw(&mut self) {
+
+    }
+
+    pub fn flip(&mut self, x: usize, y: usize) -> bool {
+        if x < self.width && y < self.height {
+            let val = self.grid[x][y] == true;
+            self.grid[x][y] ^= true;
+            return val;
+        }
+        return false;
     }
 }
